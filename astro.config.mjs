169ec3d5fa103f11e50e30e75cb99093bac7d0f7
Uri Bar Ilan <ubarilan@gmail.com>
2022-07-20 00:55:03 +0300

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    outDir: "./dist",
+    site: "https://www.ubarilan.dev",
+    integrations: [tailwind()],
+});
