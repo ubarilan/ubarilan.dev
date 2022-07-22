026820f6e369a86e2011b15de6d7d98dbb38b858
Uri Bar Ilan <ubarilan@gmail.com>
2022-07-22 15:42:45 +0300

    Added footer with all links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
     outDir: "./dist",
     site: "https://www.ubarilan.dev",
-    integrations: [tailwind()],
+    integrations: [tailwind(), react()],
 });
---
+++ src/components/react/FooterIcon.tsx
@@ -0,0 +1,28 @@
+import { Componenet } from "react";
+import {
+    faGithub,
+    faLinkedin,
+    faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const icons = {
+    github: faGithub,
+    linkedin: faLinkedin,
+    discord: faDiscord,
+};
+
+export default function FooterIcon({
+    icon,
+}: {
+    icon: keyof typeof icons;
+}): Componenet {
+    if (!Object.keys(icons).includes(icon)) return <></>;
+
+    return (
+        <FontAwesomeIcon
+            className="mr-1 h-6 w-6 fill-current sm:h-8 sm:w-8"
+            icon={icons[icon]}
+        />
+    );
+}
