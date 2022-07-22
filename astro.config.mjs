b4243a1385b76231621df67a61ddd1eb17d78acb
Uri Bar Ilan <ubarilan@gmail.com>
2022-07-22 19:00:46 +0300

    Added sitemap and turned on experimental features (unofficial integrations)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
+import sitemap from "astro-sitemap";
 
 export default defineConfig({
     outDir: "./dist",
     site: "https://www.ubarilan.dev",
-    integrations: [tailwind(), robotsTxt()],
+    experimental: {
+        integrations: true,
+    },
+    integrations: [tailwind(), robotsTxt(), sitemap()],
     vite: {
         ssr: {
             external: ["svgo"],
