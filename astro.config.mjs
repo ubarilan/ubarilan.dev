7d555578ef926564bba087c37295ea725b791d1a
Uri Bar Ilan <ubarilan@gmail.com>
2022-07-22 23:35:30 +0300

    Added main page and some animation to footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "astro-sitemap";
 
@@ -9,7 +10,7 @@ export default defineConfig({
     experimental: {
         integrations: true,
     },
-    integrations: [tailwind(), robotsTxt(), sitemap()],
+    integrations: [tailwind(), robotsTxt(), sitemap(), react()],
     vite: {
         ssr: {
             external: ["svgo"],
