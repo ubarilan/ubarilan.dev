c4929ae924a75fc0ea55f3841dca1695020feee2
Uri Bar Ilan <ubarilan@gmail.com>
2022-07-22 23:49:40 +0300

    Switched to official Astro sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
-import sitemap from "astro-sitemap";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-    outDir: "./dist",
-    site: "https://www.ubarilan.dev",
-    experimental: {
-        integrations: true,
-    },
-    integrations: [tailwind(), robotsTxt(), sitemap(), react()],
-    vite: {
-        ssr: {
-            external: ["svgo"],
-        },
-    },
+  outDir: "./dist",
+  site: "https://www.ubarilan.dev",
+  experimental: {
+    integrations: true
+  },
+  integrations: [tailwind(), robotsTxt(), react(), sitemap()],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  }
 });
