dd6ab9aa5cc416bf4551d8e4d30a99df4f84a284
Uri Bar Ilan <ubarilan@gmail.com>
2022-07-22 18:45:51 +0300

    Added robots.txt integration for better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
     outDir: "./dist",
     site: "https://www.ubarilan.dev",
-    integrations: [tailwind()],
+    integrations: [tailwind(), robotsTxt()],
     vite: {
         ssr: {
             external: ["svgo"],
