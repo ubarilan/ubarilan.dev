08b660a268aca9d22b3c3d7404e17adfec344729
Uri Bar Ilan <ubarilan@gmail.com>
2022-07-22 18:36:36 +0300

    Converted FooterIcon to Astro code because of some build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 export default defineConfig({
     outDir: "./dist",
     site: "https://www.ubarilan.dev",
-    integrations: [tailwind(), react()],
+    integrations: [tailwind()],
+    vite: {
+        ssr: {
+            external: ["svgo"],
+        },
+    },
 });
